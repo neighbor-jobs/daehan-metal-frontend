0a728c2a11824f984337c5993b4f312167571d48
jeong617 <loodol99@naver.com>
2025-03-07 04:48:04 +0900

    feat: component props가 mui component props 상속하게 변경

---
+++ src/components/AutocompleteWithLabel.tsx
@@ -0,0 +1,47 @@
+import {Autocomplete, Box, InputLabel, TextField} from '@mui/material';
+
+interface AutocompleteWithTopLabelProps {
+  labelPosition: 'left' | 'top';
+  label: string;
+  items: string[];
+  onChange?: (value: string) => void;
+}
+
+const AutocompleteWithLabel = ({
+                                    label,
+                                    items,
+                                    labelPosition,
+                                    onChange
+}: AutocompleteWithTopLabelProps):React.JSX.Element => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: labelPosition === 'top' ? 'column' : 'row',
+        alignItems: labelPosition === 'top' ? '' : 'center',
+      }}
+    >
+      <InputLabel sx={{
+        fontSize: 'small',
+        width: labelPosition === 'left' ? '20%' : '100%',
+      }}>
+        {label}
+      </InputLabel>
+      <Autocomplete
+        freeSolo
+        options={items.map((option) => option)}
+        onChange={(_, newValue) => {
+          if (onChange) onChange(newValue || '');
+        }}
+        renderInput={(params) =>
+          <TextField {...params}
+                     size='small'
+                     sx={{minWidth: 150}}
+          />
+        }
+      />
+    </Box>
+  )
+}
+
+export default AutocompleteWithLabel;
