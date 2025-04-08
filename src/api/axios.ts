f1d8e967d67ea3b02990217f16f28f0c17eed101
KangMins <rkdalsdl98@naver.com>
2025-04-08 23:42:34 +0900

    2025/04/07

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -9,7 +9,7 @@ if (!VITE_API_URL_NGROK) {
 
 // 기본 설정으로 axios 인스턴스 생성
 const axiosInstance = axios.create({
-  baseURL: VITE_API_URL_NGROK || 'http://localhost:5173',
+  baseURL: VITE_API_URL_NGROK || 'http://localhost:3000',
   timeout: 30000,
   withCredentials: true,
   headers: {
