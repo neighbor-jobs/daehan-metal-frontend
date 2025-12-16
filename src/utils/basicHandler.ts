ac47ac653988367a5dad24661508defbe0aeedb5
jeong617 <loodol99@naver.com>
2025-12-16 20:19:51 +0900

    feat: util에 input text에 개행문자 삽입해주는 함수 추가

--- src/utils/basicHandler.ts
+++ src/utils/basicHandler.ts
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const handleBasicInputChange = (
   event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   setter: React.Dispatch<React.SetStateAction<any>>
@@ -5,3 +7,10 @@ export const handleBasicInputChange = (
   const { name, value } = event.target;
   setter((prev) => ({ ...prev, [name]: value }));
 };
+
+/** input text에 개행문자 삽입해주는 함수 **/
+export const getValueWithNewLine = (e: React.KeyboardEvent<HTMLInputElement>): string => {
+  const target = e.currentTarget;
+  const {selectionStart = 0, selectionEnd = 0, value} = target;
+  return value.slice(0, selectionStart) + '\n' + value.slice(selectionEnd);
+}
